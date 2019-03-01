b8754d026c3d7fd1682663ec8521eff82a794bc4
Ipseeta <ipseeta.pkar@gmail.com>
2019-03-02 00:13:09 +0530

    Remove: views folder

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const logger = require('morgan');
 const featureRouter = require('./routes/feature');
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
